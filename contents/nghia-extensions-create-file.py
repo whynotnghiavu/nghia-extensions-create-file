65b52c011c0df21eecb0f44fbf8088ab3d222bc1
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-20 23:21:48 +0700

    update nghia-extensions-create-file.py

---
+++ contents/nghia-extensions-create-file.py
@@ -0,0 +1,28 @@
+import os
+
+def create_folder(folder_path):
+    for i in range(START, END):
+        subfolder_name = f"KhoaHoc{i}"
+        subfolder_path = os.path.join(folder_path, subfolder_name)
+        os.makedirs(subfolder_path, exist_ok=True)  
+        print(f"Created folder: {subfolder_path}")
+
+folder_path = r"input-nghia-extensions-create-file"
+
+START = 1
+END = 100 
+
+
+
+
+
+
+
+
+
+if not os.path.exists(folder_path):
+    print(f"Không có thư mục {folder_path}")
+    # os.mkdir(folder_path)
+
+
+create_folder(folder_path)
