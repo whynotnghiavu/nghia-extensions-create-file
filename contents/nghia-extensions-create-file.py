d70f770b252cbb1ddaae65831f0b8a483276cd31
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-20 23:24:13 +0700

    update nghia-extensions-create-file.py

--- contents/nghia-extensions-create-file.py
+++ contents/nghia-extensions-create-file.py
@@ -1,11 +1,19 @@
 import os
 
-def create_folder(folder_path):
+def create_file(folder_path):
     for i in range(START, END):
-        subfolder_name = f"KhoaHoc{i}"
-        subfolder_path = os.path.join(folder_path, subfolder_name)
-        os.makedirs(subfolder_path, exist_ok=True)  
-        print(f"Created folder: {subfolder_path}")
+        file_name = f"KhoaHoc{i}.txt"
+        file_path = os.path.join(folder_path, file_name)
+        # os.makedirs(file_path, exist_ok=True)  
+        # os.makedirs(file_path, exist_ok=True)  
+        # os.makedirs(file_path, exist_ok=True)  
+        # os.makedirs(file_path, exist_ok=True)  
+        os.makedirs(file_path, exist_ok=True)  
+
+
+
+
+        print(f"Created file: {file_path}")
 
 folder_path = r"input-nghia-extensions-create-file"
 
@@ -32,4 +40,4 @@ if not os.path.exists(folder_path):
     # os.mkdir(folder_path)
 
 
-create_folder(folder_path)
+create_file(folder_path)
